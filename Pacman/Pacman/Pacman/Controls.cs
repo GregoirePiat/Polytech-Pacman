e4237eefd1954c584d1d74339e2f451a3a6a26c0
Agory <gory.alexandre02@gmail.com>
2016-01-06 14:18:43 +0100

    Controls

--- Pacman/Pacman/Pacman/Controls.cs
+++ Pacman/Pacman/Pacman/Controls.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +8,32 @@ namespace Pacman
 {
     class Controls
     {
+        public const Keys JOUEUR_UP = Keys.UP;
+        public const Keys JOUEUR_DOWN = Keys.DOWN;
+        public const Keys JOUEUR_LEFT = Keys.LEFT;
+        public const Keys JOUEUR_RIGHT = Keys.RIGHT;
+
+        // Vérifie si le joueur passé en paramètre a effectué l'action "monter la raquette"
+        public static Boolean CheckActionUp(Joueur joueur)
+        {
+            Boolean checkActionUp = false;
+            KeyboardState keyboard = Keyboard.GetState();
+            if (keyboard.IsKeyDown(JOUEUR_UP)) {
+
+            }
+            if (keyboard.IsKeyDown(JOUEUR_UP))
+            {
+            }
+            if (keyboard.IsKeyDown(JOUEUR_UP))
+            {
+            }
+            if (keyboard.IsKeyDown(JOUEUR_UP))
+            {
+            }
+
+            return checkActionUp;
+        }
+
+        
     }
 }
--- Pacman/Pacman/Pacman/Joueur.cs
+++ Pacman/Pacman/Pacman/Joueur.cs
@@ -3,12 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-
-
 namespace Pacman
 {
-    class MoteurJeu
+    class Joueur
     {
-        
     }
 }
